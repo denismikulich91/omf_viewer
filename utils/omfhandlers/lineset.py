ea7bfa76989c25cb9efd01869e2e1cc1cc372d64
dmh5 <denismikulich91@gmail.com>
2024-01-07 21:21:11 +0000

    fixed lineset_to_pandas_dataframe function to export lineset into pandas dataframe

--- utils/omfhandlers/lineset.py
+++ utils/omfhandlers/lineset.py
@@ -11,29 +11,32 @@ class LinesHandler:
         self.id_field = id_field
 
     def lineset_to_pandas_dataframe(self):
-        # print(len(end_points))
-        #     'start_x': [point[0] for point in start_points],
-        #     'end_x': [point[0] for point in end_points],
-        #     'start_y': [point[1] for point in start_points],
-        #     'end_y': [point[1] for point in end_points],
-        #     'start_z': [point[2] for point in start_points],
-        #     'end_z': [point[2] for point in end_points],
 
         lineset_df = pd.DataFrame()
 
         for attribute in self.lines.attributes:
             lineset_df[attribute.name] = attribute.array.array
+        offset = 0
+        for index in range(len(self.lines.vertices) - 1):
 
-        for index, row in lineset_df.iterrows():
             if index < len(lineset_df) - 1:
-                id = row[self.id_field]
+
+                id = lineset_df.iloc[index][self.id_field]
                 next_id = lineset_df.iloc[index + 1][self.id_field]
-                if id == next_id:
-                    lineset_df.at[index, "x_start"] = self.lines.vertices[index][0]
-                    lineset_df.at[index, "y_start"] = self.lines.vertices[index][1]
-                    lineset_df.at[index, "z_start"] = self.lines.vertices[index][2]
-                    lineset_df.at[index, "x_end"] = self.lines.vertices[index + 1][0]
-                    lineset_df.at[index, "y_end"] = self.lines.vertices[index + 1][1]
-                    lineset_df.at[index, "z_end"] = self.lines.vertices[index + 1][2]
+                if id != next_id:
+                    offset += 1
+
+                lineset_df.at[index, "x_start"] = self.lines.vertices[index + offset][0]
+                lineset_df.at[index, "y_start"] = self.lines.vertices[index + offset][1]
+                lineset_df.at[index, "z_start"] = self.lines.vertices[index + offset][2]
+                lineset_df.at[index, "x_end"] = self.lines.vertices[index + 1 + offset][0]
+                lineset_df.at[index, "y_end"] = self.lines.vertices[index + 1 + offset][1]
+                lineset_df.at[index, "z_end"] = self.lines.vertices[index + 1 + offset][2]
+        lineset_df.at[len(lineset_df) - 1, "x_start"] = self.lines.vertices[-2][0]
+        lineset_df.at[len(lineset_df) - 1, "y_start"] = self.lines.vertices[-2][1]
+        lineset_df.at[len(lineset_df) - 1, "z_start"] = self.lines.vertices[-2][2]
+        lineset_df.at[len(lineset_df) - 1, "x_end"] = self.lines.vertices[-1][0]
+        lineset_df.at[len(lineset_df) - 1, "y_end"] = self.lines.vertices[-1][1]
+        lineset_df.at[len(lineset_df) - 1, "z_end"] = self.lines.vertices[-1][2]
 
         return lineset_df
